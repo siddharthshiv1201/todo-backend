2802438950ecae28b3544d86c7dd873d4b73b2fb
siddharthshiv1201 <shivsiddharth1201@gmail.com>
2025-07-23 09:48:49 +0530

    new

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,29 +1,34 @@
 const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
 
-const generateToken = (userId) => {
-  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '7d' });
+const generateToken = (user) => {
+  return jwt.sign(
+    { userId: user._id, email: user.email, name: user.name || "Anonymous" },
+    process.env.JWT_SECRET,
+    { expiresIn: '7d' }
+  );
 };
 
 exports.register = async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const { email, password, name } = req.body;
 
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ message: 'User already exists' });
     }
 
-    const newUser = new User({ email, password });
+    const newUser = new User({ email, password, name });
     await newUser.save();
 
-    const token = generateToken(newUser._id);
-
+    const token = generateToken(newUser);
     res.status(201).json({ message: 'User registered', token });
+
   } catch (err) {
     res.status(500).json({ message: 'Server error', error: err.message });
   }
 };
+
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -38,13 +43,10 @@ exports.login = async (req, res) => {
       return res.status(401).json({ message: 'Invalid credentials' });
     }
 
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-      expiresIn: '7d',
-    });
-
+    const token = generateToken(user);
     res.status(200).json({ message: 'Login successful', token });
+
   } catch (err) {
     res.status(500).json({ message: 'Server error', error: err.message });
   }
 };
-
--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,36 @@
+// const mongoose = require('mongoose');
+// const bcrypt = require('bcryptjs');
+
+// const userSchema = new mongoose.Schema({
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//   },
+// });
+
+// // Password ko save se pehle hash karna
+// userSchema.pre('save', async function (next) {
+//   if (!this.isModified('password')) return next();
+//   const salt = await bcrypt.genSalt(10);
+//   this.password = await bcrypt.hash(this.password, salt);
+//   next();
+// });
+
+// module.exports = mongoose.model('User', userSchema);
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: false,
+    default: "Anonymous",
+  },
   email: {
     type: String,
     required: true,
